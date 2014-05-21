9485f8f71c8e795850dd37f2c8fc850fde975feb
Hongsen <hhsen8816@gmail.com>
2014-05-21 14:21:31 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld{
 	public static void main (String [] args){
 		System.out.println("Hello ;;;;;World");
 		System.out.println("Print hello world in this new branch!");
+		
+		System.out.println("Let's try again in this new branch!  but delete the first sop");
 	}
 }
